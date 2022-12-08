7810d19ce272cd7a5714d68537eb8d30ddbf3207
manish-techis <109833031+manish-techis@users.noreply.github.com>
2022-12-09 04:57:40 +0530

    Update CartItem.jsx

--- src/components/Common/CartItem.jsx
+++ src/components/Common/CartItem.jsx
@@ -25,7 +25,7 @@ const CartItem = ({ cart, quantity, cartId }) => {
     return (
         <>
             <div className="circle">
-                <img src={'https://res.cloudinary.com/www-techis-io/' + cart.image} />
+                <img src={'CLOUDINARY_URL=cloudinary://415376419579977:bWDbB3dXJbXTlDW59ywrzH4mN4I@dk82loasf' + cart.image} />
             </div>
             <h3>{cart.name}</h3>
             <h6>1 Ltr</h6>
